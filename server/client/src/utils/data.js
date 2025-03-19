6e34ca322ec3c3e0da763a849a78a6810f6e9517
SahilPattankude <pattankudesahil@gmail.com>
2025-03-19 20:25:05 +0530

    added render path

--- server/client/src/utils/data.js
+++ server/client/src/utils/data.js
@@ -1,7 +1,7 @@
-export const USER_API_ENDPOINT = "http://localhost:3000/api/user";
-export const JOB_API_ENDPOINT = "http://localhost:3000/api/job";
-export const APPLICATION_API_ENDPOINT = "http://localhost:3000/api/application";
-export const COMPANY_API_ENDPOINT = "http://localhost:3000/api/company";
+export const USER_API_ENDPOINT = "https://job-9xk6.onrender.com/api/user";
+export const JOB_API_ENDPOINT = "https://job-9xk6.onrender.com/api/job";
+export const APPLICATION_API_ENDPOINT = "https://job-9xk6.onrender.com/api/application";
+export const COMPANY_API_ENDPOINT = "https://job-9xk6.onrender.com/api/company";
 
 
 
